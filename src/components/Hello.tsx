31e309461dec1ad982708f7576dd19d0861ab3c1
Ben Goh <benglin@outlook.com>
2017-04-09 02:49:34 +0800

    Added style sheets

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export class Hello extends React.Component<undefined, undefined> {
     render() {
-        return <h1>Hello from React.js!</h1>;
+        return <div className="MainCaption">Hello from React.js!</div>;
     }
 }
