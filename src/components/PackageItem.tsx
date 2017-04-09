9afab1891f52e5dfe38f6b0a866f7698f5d8e103
Ben Goh <benglin@outlook.com>
2017-04-09 22:59:39 +0800

    Implemented selection state

--- src/components/PackageItem.tsx
+++ src/components/PackageItem.tsx
@@ -1,14 +1,32 @@
 import * as React from "react";
 
-export interface PackageItemProps { data: any }
-export interface PackageItemState { expanded: boolean }
+export interface SetSelectionFunc {
+    (index: number): void
+}
+
+export interface PackageItemProps {
+    index: number,
+    data: any,
+    selected: boolean,
+    setSelection: SetSelectionFunc
+}
+
+export interface PackageItemState {
+    expanded: boolean
+}
 
 export class PackageItem extends React.Component<PackageItemProps, PackageItemState> {
 
     constructor(props: PackageItemProps) {
         super(props); // Must call base class
         this.state = { expanded: false }; // Assign initial state
+
         this.toggleExpandState = this.toggleExpandState.bind(this);
+        this.packageItemClicked = this.packageItemClicked.bind(this);
+    }
+
+    packageItemClicked() {
+        this.props.setSelection(this.props.index);
     }
 
     toggleExpandState() {
@@ -28,12 +46,15 @@ export class PackageItem extends React.Component<PackageItemProps, PackageItemSt
             description = description.substr(0, 140) + "...";
         }
 
-        return (<div className="ItemContainer">
-            <div className="ItemLeftPanel">
+        // Change background style based on selection state.
+        let selectedStyle = this.props.selected ? "ItemLeftPanelSelected" : "ItemLeftPanel";
+
+        return (<div className="ItemContainer" onClick={ this.packageItemClicked }>
+            <div className={ selectedStyle } >
                 <img className="PackageIcon" src="/resources/icons/package.png" />
             </div>
             <div className="ItemRightPanel">
-                <div className="PackageCaption">{ pkg.name }<span className="PackageVersion">{ latest.version }</span></div>
+                <div className="PackageCaption">{ this.props.index }{ pkg.name }<span className="PackageVersion">{ latest.version }</span></div>
                 <div className="PackageAuthor">by { maintainer.username }</div>
                 <div className="PackageDescription">{ description }</div>
                 <div className="DescExpanderText" onClick={ this.toggleExpandState }>[{ expanderText }]</div>
--- src/components/PackageList.tsx
+++ src/components/PackageList.tsx
@@ -2,15 +2,27 @@ import * as React from "react";
 import { PackageItem } from "./PackageItem";
 
 export interface PackageListProps { packages: any }
+export interface PackageListState { selection: number }
 
-export class PackageList extends React.Component<PackageListProps, undefined> {
+export class PackageList extends React.Component<PackageListProps, PackageListState> {
+
+    constructor(props: PackageListProps) {
+        super(props);
+        this.state = { selection: -1 };
+        this.setSelection = this.setSelection.bind(this);
+    }
+
+    setSelection(index: number) {
+        this.setState({ selection: index });
+    }
 
     render() {
 
-        let packageElements: JSX.Element[] = [];
-        this.props.packages.map((pkg: any) => packageElements.push(
-            <PackageItem data={ pkg } />
-        ));
+        let index = 0;
+        let packageElements: JSX.Element[] = this.props.packages.map((pkg: any) => 
+            <PackageItem selected={ index == this.state.selection }
+                index={ index++ } data={ pkg } setSelection={ this.setSelection } />
+        );
 
         return (<div>{ packageElements }</div>);
     }
