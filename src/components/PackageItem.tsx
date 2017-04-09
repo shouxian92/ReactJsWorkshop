64926eac47be1dc9c6a6f3d2e995d4589c6f2f90
Ben Goh <benglin@outlook.com>
2017-04-09 19:56:25 +0800

    Completed initial package item rendering

--- src/components/PackageItem.tsx
+++ src/components/PackageItem.tsx
@@ -7,15 +7,17 @@ export interface PackageItemProps {
 export class PackageItem extends React.Component<PackageItemProps, undefined> {
     render() {
         let pkg = this.props.data; // A single package.
+        let latest = pkg.versions[0]; // Latest version info.
+        let maintainer = pkg.maintainers[0];
 
         return (<div className="ItemContainer">
             <div className="ItemLeftPanel">
                 <img className="PackageIcon" src="/resources/icons/package.png" />
             </div>
             <div className="ItemRightPanel">
-                <div className="PackageCaption">{ pkg.name }<span className="PackageVersion">1.0.1</span></div>
-                <div className="PackageAuthor">by Ellensi</div>
-                <div className="PackageDescription">Bla bla bla...</div>
+                <div className="PackageCaption">{ pkg.name }<span className="PackageVersion">{ latest.version }</span></div>
+                <div className="PackageAuthor">by { maintainer.username }</div>
+                <div className="PackageDescription">{ pkg.description }</div>
             </div>
         </div>);
     }
