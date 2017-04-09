98b6cadf88d3c5abb374a28ba57e9e6652a961dd
Ben Goh <benglin@outlook.com>
2017-04-09 16:39:03 +0800

    Added 'packages' end-point

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var express = require("express");
 var app = express();
+var fs = require("fs");
+
+function loadJsonFromFile(jsonPath, req, res) {
+
+  fs.readFile(jsonPath, function(err, data) {
+    if (err) {
+      res.end(err.message);
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(data.toString());
+    }
+  });
+}
 
 app.use(express.static(__dirname));
 
 app.get("/", function (req, res) {
   res.render("index.html");
+});
+
+app.get("/packages", function(req, res) {
+  loadJsonFromFile("./resources/data/package.json", req, res);
 });
 
 app.listen(3000, function () {
